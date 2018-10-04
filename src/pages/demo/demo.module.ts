88a7ee15328876d222e86849bab4069502fc35f7
Teerachai Laothong <tlaothong@gmail.com>
2018-10-04 15:05:01 +0700

    Can use component

--- src/pages/demo/demo.module.ts
+++ src/pages/demo/demo.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { DemoPage } from './demo';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   declarations: [
     DemoPage,
   ],
   imports: [
+    ComponentsModule,
     IonicPageModule.forChild(DemoPage),
   ],
 })
