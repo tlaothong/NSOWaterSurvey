b327919c2bc77328369d3c21f7f9f59e0ad01967
Teerachai Laothong <tlaothong@gmail.com>
2018-10-04 15:40:07 +0700

    Change sequence of import

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
 import { WaterSources8AComponent } from './water-sources8-a/water-sources8-a';
+import { DemoNaComponent } from './demo-na/demo-na';
 
 @NgModule({
-	declarations: [WaterSources8AComponent
+	declarations: [WaterSources8AComponent,
+    DemoNaComponent
     ],
 	imports: [
-		IonicPageModule.forChild(WaterSources8AComponent)
 	],
-	exports: [WaterSources8AComponent
+	exports: [WaterSources8AComponent,
+    DemoNaComponent
     ]
 })
 export class ComponentsModule {}
