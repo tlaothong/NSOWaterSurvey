b3b47757ea4e4e89f355cb8c035adccc35d3e6f3
woraput <woraput@kkumail.com>
2018-10-04 21:15:07 +0700

    commit diamond

---
+++ src/pages/dry-crop-planting/dry-crop-planting.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { DryCropPlantingPage } from './dry-crop-planting';
+
+@NgModule({
+  declarations: [
+    DryCropPlantingPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(DryCropPlantingPage),
+  ],
+})
+export class DryCropPlantingPageModule {}
