72e021c09c313267d802b7bc28663622de17194a
nerddy222 <nerddy.hahaha@hotmail.com>
2018-10-04 18:24:02 +0700

    Create Poolcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    ComponentsModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { WaterSources8AComponent } from './water-sources8-a/water-sources8-a';
 import { DemoNaComponent } from './demo-na/demo-na';
+import { PoolComponent } from './pool/pool';
 
 @NgModule({
 	declarations: [WaterSources8AComponent,
-    DemoNaComponent
+    DemoNaComponent,
+    PoolComponent
     ],
 	imports: [
         IonicPageModule.forChild(WaterSources8AComponent)
 	],
 	exports: [WaterSources8AComponent,
-    DemoNaComponent
+    DemoNaComponent,
+    PoolComponent
     ]
 })
 export class ComponentsModule {}
