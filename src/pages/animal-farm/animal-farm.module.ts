0c53998b6b6f875b9d2592b4e15f7b58f75deca8
tatae <tachapong999.gmail.com>
2018-10-04 21:32:01 +0700

    animal-farm

--- src/pages/animal-farm/animal-farm.module.ts
+++ src/pages/animal-farm/animal-farm.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { AnimalFarmPage } from './animal-farm';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { AnimalFarmPage } from './animal-farm';
   ],
   imports: [
     IonicPageModule.forChild(AnimalFarmPage),
+    ComponentsModule,
   ],
 })
 export class AnimalFarmPageModule {}
