1a03f5c56d021991e59cb7f8d82ddac5d1ad6da8
nerddy222 <nerddy.hahaha@hotmail.com>
2018-10-08 18:24:52 +0700

    Edit PoolComponent, Add water-activity Component, Add water-problem Component

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -2,46 +2,52 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { WaterSources8AComponent } from './water-sources8-a/water-sources8-a';
 import { DemoNaComponent } from './demo-na/demo-na';
-import { PoolComponent } from './pool/pool';
+import { PumpComponent } from './pump/pump';
 import { WaterSources8BComponent } from './water-sources8-b/water-sources8-b';
 import { WaterSources9Component } from './water-sources9/water-sources9';
-import { EnginewaterActivity4Component } from './enginewater-activity4/enginewater-activity4';
-import { EnginewaterActivity6Component } from './enginewater-activity6/enginewater-activity6';
 
 import { FieldAreaComponent } from './field-area/field-area';
 import { PoolAreaComponent } from './pool-area/pool-area';
 import { RectanglePoolComponent } from './rectangle-pool/rectangle-pool';
 import { CirclePoolComponent } from './circle-pool/circle-pool';
+import { WaterActivity5Component } from './water-activity5/water-activity5';
+import { WaterActivity6Component } from './water-activity6/water-activity6';
+import { WaterProblem4Component } from './water-problem4/water-problem4';
+import { WaterProblem6Component } from './water-problem6/water-problem6';
 
 @NgModule({
 	declarations: [WaterSources8AComponent,
     DemoNaComponent,
-    PoolComponent,
+    PumpComponent,
     WaterSources8BComponent,
     WaterSources9Component,
-    EnginewaterActivity4Component,
-    EnginewaterActivity6Component,
-    
     FieldAreaComponent,
     PoolAreaComponent,
     RectanglePoolComponent,
-    CirclePoolComponent
+    CirclePoolComponent,
+    WaterActivity5Component,
+    WaterActivity6Component,
+    WaterActivity5Component,
+    WaterProblem4Component,
+    WaterProblem6Component
     ],
 	imports: [
         IonicPageModule.forChild(WaterSources8AComponent)
 	],
 	exports: [WaterSources8AComponent,
     DemoNaComponent,
-    PoolComponent,
+    PumpComponent,
     WaterSources8BComponent,
     WaterSources9Component,
-    EnginewaterActivity4Component,
-    EnginewaterActivity6Component,
-    
     FieldAreaComponent,
     PoolAreaComponent,
     RectanglePoolComponent,
-    CirclePoolComponent
+    CirclePoolComponent,
+    WaterActivity5Component,
+    WaterActivity6Component,
+    WaterActivity5Component,
+    WaterProblem4Component,
+    WaterProblem6Component
     ]
 })
 export class ComponentsModule {}
---
+++ src/components/water-activity5/water-activity5.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the WaterActivity5Component component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'water-activity5',
+  templateUrl: 'water-activity5.html'
+})
+export class WaterActivity5Component {
+
+  text: string;
+
+  constructor() {
+    console.log('Hello WaterActivity5Component Component');
+    this.text = 'Hello World';
+  }
+
+}
