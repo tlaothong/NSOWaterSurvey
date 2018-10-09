f410676b1e98ea051d4cba3712c8d6b2fef9f2e1
Kritsana <tragoolphrom@gmail.com>
2018-10-09 15:59:57 +0700

    implement DlgFieldArea

---
+++ src/pages/dlg-field-area/dlg-field-area.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { FormGroup } from '@angular/forms';
+
+/**
+ * Generated class for the DlgFieldAreaPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-dlg-field-area',
+  templateUrl: 'dlg-field-area.html',
+})
+export class DlgFieldAreaPage {
+
+  public FormItem: FormGroup;
+  public text: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController) {
+    this.FormItem = navParams.get('FormItem');
+    this.text = navParams.get("iTitle");
+  }
+
+  public closeDialog() {
+    this.viewCtrl.dismiss();
+  }
+
+  public okDialog() {
+    this.viewCtrl.dismiss(this.FormItem);
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DlgFieldAreaPage');
+  }
+
+}
