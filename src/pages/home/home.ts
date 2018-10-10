1a278d912605e9732f85b032bfb8b80c8786d44b
Teerachai Laothong <tlaothong@gmail.com>
2018-10-10 10:44:50 +0700

    List pages in Home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,42 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  pages: Array<{ title: string, component: any }>;
 
+  constructor(public navCtrl: NavController) {
+    // used for an example of ngFor and navigation
+    this.pages = [
+      // test push
+      { title: 'Zero', component: "ZeroPage" },
+      { title: 'Demo', component: "DemoPage" },
+      { title: 'Rice', component: "RicePage" },
+      { title: 'Mushroom', component: "MushroomPage" },
+      { title: 'Plumbing', component: "PlumbingPage" },
+      { title: 'GroundWater', component: "GroundWaterPage" },
+      { title: 'River', component: "RiverPage" },
+      { title: 'FlowerCrop', component: "FlowerCropPage" },
+      { title: 'HerbsPlant', component: "HerbsPlantPage" },
+      { title: 'AnimalFarm', component: "AnimalFarmPage" },
+      { title: 'Factorial', component: "FactorialPage" },
+      { title: 'Disaster', component: "DisasterPage" },
+      { title: 'Residential', component: "ResidentialPage" },
+      { title: 'Buying', component: "BuyingPage" },
+      { title: 'Disasterous', component: "DisasterousPage" },
+      { title: 'Irrigation', component: "IrrigationPage" },
+      { title: 'Rain', component: "RainPage" },
+      { title: 'Dry-CropPlanting', component: "DryCropPlantingPage" },
+      { title: 'RubberTree', component: "RubberTreePage" },
+      { title: 'PerennialPlanting', component: "PerennialPlantingPage" },
+      { title: 'WaterAnimalPlanting', component: "WaterAnimalPlantingPage" },
+      { title: 'User', component: "UserPage" },
+      { title: 'Pool', component: "PoolPage" },
+    ];
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.navCtrl.push(page.component);
   }
 
 }
