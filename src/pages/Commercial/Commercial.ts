c7ddbff6a6b8c7d3b55dd49c7bc7708e32afcc2b
powerofwind <powerofwind10@gmail.com>
2018-10-10 15:33:22 +0700

    change Title-name at HomePage

--- src/pages/Commercial/Commercial.ts
+++ src/pages/Commercial/Commercial.ts
@@ -14,6 +14,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'Commercial.html',
 })
 export class CommercialPage {
+  
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
