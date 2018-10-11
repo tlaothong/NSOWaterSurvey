84fb2baa797fd451157451fdfc6c7de32e17a945
guntza10 <guntza22@hotmail.com>
2018-10-11 15:41:14 +0700

    validate machine-water

--- src/components/machine-water/machine-water.ts
+++ src/components/machine-water/machine-water.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { ISubmitRequestable } from './../../shared/ISubmitRequestable';
+import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 /**
  * Generated class for the MachineWaterComponent component.
@@ -10,13 +13,32 @@ import { Component } from '@angular/core';
   selector: 'machine-water',
   templateUrl: 'machine-water.html'
 })
-export class MachineWaterComponent {
+export class MachineWaterComponent implements ISubmitRequestable {
+
 
   text: string;
-
-  constructor() {
+  @Input() public FormItem: FormGroup;
+  private submitRequested: boolean;
+  constructor(public fb: FormBuilder) {
     console.log('Hello MachineWaterComponent Component');
     this.text = 'Hello World';
+
+    this.FormItem = this.fb.group({
+      'EnergySource': [''],
+      'PumpType': [''],
+      'HorsePower': [''],
+      'SuctionPipeSize': [''],
+      'PipelineSize': ['']
+    });
   }
+  // core 
+  submitRequest() {
+    this.submitRequested = true;
+  }
+  public isValid(name: string): boolean {
+    var ctrl = this.FormItem.get(name);
+    return ctrl.invalid && (ctrl.dirty || this.submitRequested);
+  }
+
 
 }
