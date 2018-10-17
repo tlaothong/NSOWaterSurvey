0ff3278b653a656364efac4bda31f7722a5b2116
woraput <woraput@kkumail.com>
2018-10-17 11:54:47 +0700

    crocodile component work

--- src/components/crocodile-farming/crocodile-farming.ts
+++ src/components/crocodile-farming/crocodile-farming.ts
@@ -1,6 +1,9 @@
-import { Component, Input } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, Input, ViewChildren, AfterViewInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { FrogFarmingComponent } from '../frog-farming/frog-farming';
+import { PoolAreaComponent } from '../pool-area/pool-area';
+import { ISubmitRequestable } from '../../shared/ISubmitRequestable';
+import { WaterSources9Component } from '../water-sources9/water-sources9';
 
 /**
  * Generated class for the CrocodileFarmingComponent component.
@@ -12,63 +15,98 @@ import { FrogFarmingComponent } from '../frog-farming/frog-farming';
   selector: 'crocodile-farming',
   templateUrl: 'crocodile-farming.html'
 })
-export class CrocodileFarmingComponent {
+export class CrocodileFarmingComponent implements AfterViewInit ,ISubmitRequestable {
 
   @Input() public FormItem: FormGroup;
-  text: string;
+  @Input("headline") public text: string;
+  @Input('no') no :string;
+
+  @ViewChildren(PoolAreaComponent) private poolArea: PoolAreaComponent[];
+  
+  @ViewChildren(WaterSources9Component) private waterSources9 : WaterSources9Component[];
+
   private submitRequested: boolean;
 
   constructor(public fb: FormBuilder) {
     console.log('Hello CrocodileFarmingComponent Component');
     this.text = 'Hello World';
 
-    this.FormItem = FrogFarmingComponent.CreateFormGroup(fb);
+    this.FormItem = CrocodileFarmingComponent.CreateFormGroup(fb);
   }
+
+  
+  ngAfterViewInit(): void {
+    this.setupFieldCountChanges()
+  }
+  
 
   public static CreateFormGroup(fb: FormBuilder): FormGroup {
     return fb.group({
       "doing": [null, Validators.required],
-      "depression": [null, Validators.required],
-      "other": [null, Validators.required],
+      "depression": [false, Validators.required],
+      "other": [false, Validators.required],
       "fieldCount": [null, Validators.required],
       "fieldsAreSameSize": [null, Validators.required],
-      "fields": fb.group({
-        "shape": [null, Validators.required],
-        "area": fb.group({
-          "rai": [null, Validators.required],
-          "ngan": [null, Validators.required],
-          "sqWa": [null, Validators.required]
-        }),
-        "depth": [null, Validators.required],
-        "rectangle": fb.group({
-          "width": [null, Validators.required],
-          "length": [null, Validators.required],
-          "_id": [null, Validators.required]
-        }),
-        "diameter": [null, Validators.required]
-      }),
+
+      "fields": fb.array([]),
+
       "animalsCount": [null, Validators.required],
-      "waterSources": fb.group({
-        "plumbing": [null, Validators.required],
-        "underGround": [null, Validators.required],
-        "pool": [null, Validators.required],
-        "river": [null, Validators.required],
-        "irrigation": [null, Validators.required],
-        "rain": [null, Validators.required],
-        "buying": [null, Validators.required],
-        "rainingAsIs": [null, Validators.required],
-        "hasOther": [null, Validators.required],
-        "other": [null, Validators.required]
-      }),
+      'waterSources': WaterSources9Component.CreateFormGroup(fb)
+     
+    
     });
+    
   }
 
   submitRequest() {
     this.submitRequested = true;
+    this.poolArea.forEach(it => it.submitRequest());
+    this.waterSources9.forEach(it=>it.submitRequest());
   }
 
+  
   public isValid(name: string): boolean {
     var ctrl = this.FormItem.get(name);
     return ctrl.invalid && (ctrl.touched || this.submitRequested);
   }
+  
+
+  private setupFieldCountChanges() {
+
+    const componentFormArray: string = "fields";
+    const componentCount: string = "fieldCount";
+
+    var onComponentCountChanges = () => {
+      var fields = (this.FormItem.get(componentFormArray) as FormArray).controls || [];
+      var fieldCount = this.FormItem.get(componentCount).value || 0;
+      var field = this.fb.array([]);
+
+      fieldCount = Math.max(0, fieldCount);
+
+      for (let i = 0; i < fieldCount; i++) {
+        var ctrl = null;
+        if (i < fields.length) {
+          const fld = fields[i];
+          ctrl = fld;
+        } else {
+          ctrl = PoolAreaComponent.CreateFormGroup(this.fb);
+        }
+
+        field.push(ctrl);
+      }
+      this.FormItem.setControl(componentFormArray, field);
+    };
+
+    this.FormItem.get(componentCount).valueChanges.subscribe(it => onComponentCountChanges());
+
+    onComponentCountChanges();
+  }
+
+
+
+
+
+
+
+
 }
