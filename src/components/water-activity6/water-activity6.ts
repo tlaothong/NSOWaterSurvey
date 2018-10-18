c35a0dd00ec39c36b1235027aaa0c346b05a0147
tatae <tachapong999.gmail.com>
2018-10-18 15:40:18 +0700

    implementFormwater-activity6

--- src/components/water-activity6/water-activity6.ts
+++ src/components/water-activity6/water-activity6.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 /**
  * Generated class for the WaterActivity6Component component.
@@ -9,15 +10,28 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'water-activity6',
   templateUrl: 'water-activity6.html'
+  
 })
 export class WaterActivity6Component {
 
   @Input('headline') public text: string;
   @Input('headline2') public text2: string;
+  @Input() public FormItem: FormGroup;
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
     console.log('Hello WaterActivity6Component Component');
     this.text = 'Hello World';
+    this.FormItem = WaterActivity6Component.CreateFormGroup(fb);
+  }
+  public static CreateFormGroup(fb: FormBuilder): FormGroup {
+    return fb.group({
+      'drink': ['', Validators.required],
+      'plant': ['', Validators.required],
+      'farm': ['', Validators.required],
+      'agriculture': ['', Validators.required],
+      'product': ['', Validators.required],
+      'service': ['', Validators.required]
+    });
   }
 
 }
