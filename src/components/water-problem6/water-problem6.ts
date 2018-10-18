7ff0ba3600d8a0167ebf6eb79fdaf1c84f1471f0
woraput <woraput@kkumail.com>
2018-10-18 15:32:35 +0700

    validate checkbox  water problem 6

--- src/components/water-problem6/water-problem6.ts
+++ src/components/water-problem6/water-problem6.ts
@@ -1,6 +1,6 @@
 import { WaterActivity6Component } from './../water-activity6/water-activity6';
 import { Component, Input } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ValidatorFn, ValidationErrors, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'water-problem6',
@@ -24,12 +24,14 @@ export class WaterProblem6Component {
 
   public static CreateFormGroup(fb: FormBuilder): FormGroup {
     return fb.group({
-      'turbidWater': [null, Validators.required],
-      'saltWater': [null, Validators.required],
-      'smell': [null, Validators.required],
-      'filmOfOil': [null, Validators.required],
-      'fogWater': [null, Validators.required],
-      'hardWater': [null, Validators.required],
+      'turbidWater': [false, Validators.required],
+      'saltWater': [false, Validators.required],
+      'smell': [false, Validators.required],
+      'filmOfOil': [false, Validators.required],
+      'fogWater': [false, Validators.required],
+      'hardWater': [false, Validators.required],
+    },{
+      validator: WaterProblem6Component.checkAnyOrOther()
     });
   }
 
@@ -41,6 +43,36 @@ export class WaterProblem6Component {
 
   public isValid(name: string): boolean {
     var ctrl = this.FormItem.get(name);
+    if (name == 'anycheck') {
+      ctrl = this.FormItem;
+      return ctrl.errors && ctrl.errors.anycheck && (ctrl.touched || this.submitRequested);
+    } 
+    // else if (name == 'other') {
+    //   return this.FormItem.errors && this.FormItem.errors.other && (ctrl.touched || this.submitRequested);
+    // }
     return ctrl.invalid && (ctrl.touched || this.submitRequested);
   }
+
+  public static checkAnyOrOther(): ValidatorFn {
+    return (c: AbstractControl): ValidationErrors | null => {
+      const turbidWater = c.get('turbidWater');
+      const saltWater = c.get('saltWater');
+      
+      const smell = c.get('smell');
+      const filmOfOil = c.get('filmOfOil');
+      const fogWater = c.get('fogWater');
+      const hardWater = c.get('hardWater');
+      
+
+      if (!turbidWater.value && !saltWater.value && !smell.value && !filmOfOil.value
+        && !fogWater.value && !hardWater.value) {
+        return { 'anycheck': true };
+      } 
+      // else if (hasOther.value == true && (!other.value || other.value.trim() == '')) {
+      //   return { 'other': true };
+      // }
+      return null;
+    }
+  }
+
 }
