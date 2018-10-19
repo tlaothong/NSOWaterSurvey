26c322c9913bdcc175538f3900536aafe89b79a9
nerddy222 <nerddy.hahaha@hotmail.com>
2018-10-19 11:04:05 +0700

    Edit validate table-check-item-count conponent

--- src/components/table-check-item-count/table-check-item-count.ts
+++ src/components/table-check-item-count/table-check-item-count.ts
@@ -1,6 +1,6 @@
 import { Component, Input, AfterViewInit } from '@angular/core';
 import { ModalController } from 'ionic-angular';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, ValidatorFn, ValidationErrors, AbstractControl, Validators } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 import { ISubmitRequestable } from '../../shared/ISubmitRequestable';
 
@@ -31,8 +31,10 @@ export class TableCheckItemCountComponent implements AfterViewInit, ISubmitReque
 
   public static CreateFormGroup(fb: FormBuilder): FormGroup {
     return fb.group({
-      'hasItem': false,
-      'itemCount': 0
+      'hasItem': [false,Validators.required],
+      'itemCount': [null,Validators.required]
+    }, {
+      validator: TableCheckItemCountComponent.checkAnyOrOther()
     });
   }
 
@@ -62,7 +64,22 @@ export class TableCheckItemCountComponent implements AfterViewInit, ISubmitReque
 
   public isValid(name: string): boolean {
     var ctrl = this.FormItem.get(name);
+    if (name == 'anycheck') {
+      ctrl = this.FormItem;
+      return ctrl.errors && ctrl.errors.anycheck && (ctrl.touched || this.submitRequested);
+    } 
     return ctrl.invalid && (ctrl.touched || this.submitRequested);
   }
 
+  public static checkAnyOrOther(): ValidatorFn {
+    return (c: AbstractControl): ValidationErrors | null => {
+      const hasItem = c.get('hasItem');
+     
+
+      if (!hasItem.value) {
+        return { 'anycheck': true };
+      } 
+      return null;
+    }
+  }
 }
