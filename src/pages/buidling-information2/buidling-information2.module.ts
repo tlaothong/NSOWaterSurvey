1726755983117b23d4a6a294f84e2e630433df1e
unravel07 <mick_ben10@hotmail.com>
2018-10-25 10:15:19 +0700

    generate buidingInformation page

---
+++ src/pages/buidling-information2/buidling-information2.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { BuidlingInformation2Page } from './buidling-information2';
+
+@NgModule({
+  declarations: [
+    BuidlingInformation2Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(BuidlingInformation2Page),
+  ],
+})
+export class BuidlingInformation2PageModule {}
