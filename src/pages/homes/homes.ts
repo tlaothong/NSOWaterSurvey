8ce57813b8604b94b4ea0eacb465d6d07e0b6456
tatae <tachapong999.gmail.com>
2018-10-26 15:51:01 +0700

    add segment

--- src/pages/homes/homes.ts
+++ src/pages/homes/homes.ts
@@ -14,6 +14,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'homes.html',
 })
 export class HomesPage {
+  office: string = "building";
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
