dc82ebfcf0b14c06af4183ee901c9ae2aca69632
maskmeka <agapevb35@gmail.com>
2018-11-06 12:15:58 +0700

    edit _id

--- src/pages/rice/rice.ts
+++ src/pages/rice/rice.ts
@@ -35,7 +35,7 @@ export class RicePage {
       'doing': [null, Validators.required],
       'fieldCount': [null, Validators.required],
       'fields': this.fb.array([]),
-      "_id": null,
+      '_id': [null],
     });
 
     this.setupFieldCountChanges();
