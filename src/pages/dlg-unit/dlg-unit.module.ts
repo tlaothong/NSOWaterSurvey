83c8d185c63db092f7d68474b20489504cab072c
Teerachai Laothong <tlaothong@gmail.com>
2018-11-07 11:08:15 +0700

    Added import to Components in Unit Page

--- src/pages/dlg-unit/dlg-unit.module.ts
+++ src/pages/dlg-unit/dlg-unit.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { DlgUnitPage } from './dlg-unit';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   declarations: [
--- src/pages/unit/unit.module.ts
+++ src/pages/unit/unit.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { UnitPage } from './unit';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   declarations: [
     UnitPage,
   ],
   imports: [
+    ComponentsModule,
     IonicPageModule.forChild(UnitPage),
   ],
 })
