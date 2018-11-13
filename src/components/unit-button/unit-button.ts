1d6893b834261d425415b6bd23870072d0977881
Kritsana <tragoolphrom@gmail.com>
2018-11-13 13:53:49 +0700

    implement ui UnitPage

--- src/components/unit-button/unit-button.ts
+++ src/components/unit-button/unit-button.ts
@@ -1,71 +1,70 @@
-import { Component, Input } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { ModalController } from 'ionic-angular';
+import { UnitButtonComponent } from './../../components/unit-button/unit-button';
+import { Component, ViewChildren } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 
-/**
- * Generated class for the UnitButtonComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
+@IonicPage()
 @Component({
-  selector: 'unit-button',
-  templateUrl: 'unit-button.html'
+  selector: 'page-unit',
+  templateUrl: 'unit.html',
 })
-export class UnitButtonComponent {
+export class UnitPage {
 
-  @Input("headline") public text: string;
-  @Input() public FormItem: FormGroup;
-
+  public class: string;
+  public f: FormGroup;
   private submitRequested: boolean;
 
-  constructor(private modalCtrl: ModalController, private fb: FormBuilder) {
-    console.log('Hello UnitButtonComponent Component');
-    this.text = '';
+  @ViewChildren(UnitButtonComponent) private unitButton: UnitButtonComponent[];
 
-    // TODO: Remove this
-    this.FormItem = UnitButtonComponent.CreateFormGroup(this.fb);
-  }
-
-  public static CreateFormGroup(fb: FormBuilder): FormGroup {
-    return fb.group({
-      'subUnit': fb.group({
-        'roomNumber': [null, Validators.required],
-        'access': [null, Validators.required],
-        'hasPlumbing': [null, Validators.required],
-        'hasPlumbingMeter': [false, Validators.required],
-        'isPlumbingMeterXWA': [false, Validators.required],
-        'hasGroundWater': [null, Validators.required],
-        'hasGroundWaterMeter': [false, Validators.required],
-      }),
-      'isHouseHold': [null, Validators.required],
-      'isAgriculture': [null, Validators.required],
-      'isFactorial': [null, Validators.required],
-      'isCommercial': [null, Validators.required],
-      'comments': fb.group({
-        'at': [null],
-        'text': [null],
-      })
+  constructor(public navCtrl: NavController, public navParams: NavParams, public fb: FormBuilder) {
+    this.f = this.fb.group({
+      'amount': [9],
+      'units': this.fb.array([]),
     });
+
+    this.setupUnitsCountChanges();
   }
 
-  public showModal() {
-    const modal = this.modalCtrl.create("DlgUnitPage", { FormItem: this.FormItem });
-    modal.onDidDismiss(data => {
-      if (data) {
-        var fg = <FormGroup>data;
-        this.FormItem.setValue(fg.value);
-      }
-    });
-    modal.present();
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter UnitPage');
   }
 
-  submitRequest() {
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad UnitPage');
+  }
+
+  public handleSubmit() {
     this.submitRequested = true;
+    this.unitButton.forEach(it => it.submitRequest());
   }
 
-  public isValid(name: string): boolean {
-    var ctrl = this.FormItem.get(name);
-    return ctrl.invalid && (ctrl.touched || this.submitRequested);
+  private setupUnitsCountChanges() {
+    const componentFormArray: string = "units";
+    const componentCount: string = "amount";
+
+    var onComponentCountChanges = () => {
+      var units = (this.f.get(componentFormArray) as FormArray).controls || [];
+      var amount = this.f.get(componentCount).value || 0;
+      var farr = this.fb.array([]);
+
+      amount = Math.max(0, amount);
+
+      for (let i = 0; i < amount; i++) {
+        var ctrl = null;
+        if (i < units.length) {
+          const fld = units[i];
+          ctrl = fld;
+        } else {
+          ctrl = UnitButtonComponent.CreateFormGroup(this.fb);
+        }
+
+        farr.push(ctrl);
+      }
+      this.f.setControl(componentFormArray, farr);
+    };
+
+    this.f.get(componentCount).valueChanges.subscribe(it => onComponentCountChanges());
+
+    onComponentCountChanges();
   }
 }
