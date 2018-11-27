97a8625406f30fdfacdcbf57a1a99cede1ff1be5
aphaphon <pui.aphaphon@gmail.com>
2018-11-27 16:26:16 +0700

    add validate

--- src/pages/population/population.ts
+++ src/pages/population/population.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChildren } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { TablePopulationComponent } from '../../components/table-population/table-population';
 
 @IonicPage()
 @Component({
@@ -8,19 +9,62 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   templateUrl: 'population.html',
 })
 export class PopulationPage {
+
+  private submitRequested: boolean;
   public f: FormGroup;
 
+  @ViewChildren(TablePopulationComponent) private persons: TablePopulationComponent[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams, private fb: FormBuilder) {
-    // this.f = this.fb.group({
-    //   'population': this.fb.group({
-    //     'PersonCount': [null, Validators.required],
-    //     'Persons':
-    //   }),
-    // });
+    this.f = this.fb.group({
+      'personCount': [null, Validators.required],
+      'persons': this.fb.array([])
+    }),
+      this.setupPersonCountChanges();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PopulationPage');
+  }
+
+  public handleSubmit() {
+    this.submitRequested = true;
+    this.persons.forEach(it => it.submitRequest());
+  }
+
+  public isValid(name: string): boolean {
+    var ctrl = this.f.get(name);
+    return ctrl.invalid && (ctrl.touched || this.submitRequested);
+  }
+
+  private setupPersonCountChanges() {
+    const componentFormArray: string = "persons";
+    const componentCount: string = "personCount";
+
+    var onComponentCountChanges = () => {
+      var persons = (this.f.get(componentFormArray) as FormArray).controls || [];
+      var personCount = this.f.get(componentCount).value || 0;
+      var farr = this.fb.array([]);
+
+      personCount = Math.max(0, personCount);
+
+      for (let i = 0; i < personCount; i++) {
+        var ctrl = null;
+        if (i < persons.length) {
+          const fld = persons[i];
+          ctrl = fld;
+        } else {
+          ctrl = TablePopulationComponent.CreateFormGroup(this.fb);
+        }
+
+        farr.push(ctrl);
+      }
+      this.f.setControl(componentFormArray, farr);
+    };
+
+    this.f.get(componentCount).valueChanges.subscribe(it => onComponentCountChanges());
+
+    onComponentCountChanges();
   }
 
 }
