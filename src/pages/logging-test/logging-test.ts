2d4b633f2bf68200b72a19223db2fe63c32830d9
SorrY <so.sikarin@perfenterprise.com>
2018-12-13 11:57:01 +0700

    Implement Loging State

--- src/pages/logging-test/logging-test.ts
+++ src/pages/logging-test/logging-test.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LoggingState } from '../../states/logging/logging.reducer';
+import { Store } from '@ngrx/store';
+import { LoadUserInformation } from '../../states/logging/logging.actions';
 
 @IonicPage()
 @Component({
@@ -8,11 +11,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoggingTestPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  pages: Array<{ title: string, component: any }>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,private store: Store<LoggingState>) {
+    this.pages = [
+      { title: 'เปิดใช้งาน Tablet', component: "ScanqrPage" },
+      { title: 'First Login', component: "FirstloginPage" },
+      { title: 'Login', component: "LoginPage" },
+      
+    ];
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoggingTestPage');
+    this.store.dispatch(new LoadUserInformation());
   }
 
+  public openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.navCtrl.push(page.component);
+  }
 }
