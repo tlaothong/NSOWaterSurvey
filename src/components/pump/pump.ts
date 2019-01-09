110faeec3f8bbc7d1c839980636741354379b4a8
powerofwind <powerofwind10@gmail.com>
2019-01-09 18:04:33 +0700

    Modify Pump Page

--- src/components/pump/pump.ts
+++ src/components/pump/pump.ts
@@ -2,6 +2,10 @@ import { Component, Input } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { NavController, NavParams, ModalController } from 'ionic-angular';
 import { ISubmitRequestable } from '../../shared/ISubmitRequestable';
+import { surfacePumpTypeData } from '../../models/SurfacePumpTypeData';
+import { PumpDataProvider } from '../../providers/pump-data/pump-data';
+import { surfaceWattHpData } from '../../models/SurfaceWattHpData';
+import { surfaceSuctionPipeData } from '../../models/SurfaceSuctionPipeData';
 
 @Component({
   selector: 'pump',
@@ -13,7 +17,12 @@ export class PumpComponent implements ISubmitRequestable {
   @Input() public FormItem: FormGroup;
   @Input('no') public text: string;
   private submitRequested: boolean;
-
+  public surfacePumpTypeData = surfacePumpTypeData;
+  public surfacePump: any;
+  public surfaceWattHpData: any;
+  public surfaceSuctionPipeData: any;
+  public surfacePipeLineData: any;
+  
   constructor(private modalCtrl: ModalController, public navCtrl: NavController,
     public navParams: NavParams, public fb: FormBuilder) {
     this.FormItem = PumpComponent.CreateFormGroup(this.fb);
@@ -54,4 +63,25 @@ export class PumpComponent implements ISubmitRequestable {
     return ctrl.invalid && (ctrl.touched || this.submitRequested);
   }
 
+  onChange(code:any){
+    this.surfacePump = PumpDataProvider.getSurfacePumpTypeData(code);
+  }
+
+  onChange1(name:string){
+    var code = surfacePumpTypeData.find(it=>it.name == name);
+    this.surfaceWattHpData = PumpDataProvider.getSurfaceWattHpData(code.codePumpType);
+    console.log(this.surfaceWattHpData)
+  }
+  
+  onChange2(name:string){
+    var code = surfaceWattHpData.find(it=>it.name == name);
+    this.surfaceSuctionPipeData = PumpDataProvider.getSurfaceSuctionPipeData(code.codeWattHp);
+    console.log(this.surfaceSuctionPipeData)
+  }
+
+  onChange3(name:string){
+    var code = surfaceSuctionPipeData.find(it=>it.name == name);
+    this.surfacePipeLineData = PumpDataProvider.getSurfacePipeLineData(code.codeSuctionPipe);
+    console.log(this.surfacePipeLineData);
+  }
 }
