88a176c77fdb774db82d37ba0806a3353df19227
Kritsana <tragoolphrom@gmail.com>
2019-01-10 10:55:08 +0700

    implement plantingFromMonth

--- src/components/field-rice-harvest/field-rice-harvest.ts
+++ src/components/field-rice-harvest/field-rice-harvest.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { ISubmitRequestable } from '../../shared/ISubmitRequestable';
+import { text } from '@angular/core/src/render3/instructions';
 
 @Component({
   selector: 'field-rice-harvest',
@@ -10,18 +11,12 @@ export class FieldRiceHarvestComponent implements ISubmitRequestable {
 
   @Input('i') public text: string;
   @Input() public FormItem: FormGroup;
-  @Input() public FormItemBefore: FormGroup;
+  @Input() public FormArray: FormArray;
   private submitRequested: boolean;
-
-  private beforeFrom: number;
-  private beforeThru: number;
-  private from: number;
-  private thru: number;
 
   constructor(public fb: FormBuilder) {
     this.text = 'Hello World';
     this.FormItem = FieldRiceHarvestComponent.CreateFormGroup(this.fb);
-    this.FormItemBefore = FieldRiceHarvestComponent.CreateFormGroup(this.fb);
   }
 
   public static CreateFormGroup(fb: FormBuilder): FormGroup {
@@ -42,31 +37,15 @@ export class FieldRiceHarvestComponent implements ISubmitRequestable {
     return ctrl.invalid && (ctrl.touched || this.submitRequested);
   }
 
-  public isCheck(): boolean {
-    if (this.text != "1") {
-      this.beforeFrom = this.FormItemBefore.get('plantingFromMonth').value;
-      this.beforeThru = this.FormItemBefore.get('plantingThruMonth').value;
-      this.from = this.FormItem.get('plantingFromMonth').value;
-      this.thru = this.FormItem.get('plantingThruMonth').value;
-
-      if (this.beforeThru < this.beforeFrom) {
-        if (this.from < this.beforeThru) {
-          this.from += 12;
-          this.thru += 12;
-        }
-        this.beforeThru += 12;
-      }
-      if (this.thru < this.from) {
-        this.thru += 12;
-      }
-      
-      return (this.beforeFrom < this.from && this.from < this.beforeThru) || (this.beforeFrom < this.thru && this.thru < this.beforeThru);
+  public isCheck(month: number, from: string): boolean {
+    let index = parseInt(this.text) - 1;
+    if (index > 0) {
+      let start = this.FormArray.at(0).get('plantingFromMonth').value;
+      let end = (from == "from") ? this.FormArray.at(index - 1).get('plantingThruMonth').value : this.FormItem.get('plantingFromMonth').value;
+      return (start < end) ? !(start < month && month < end) : !(start < month || month < end);
     }
     else {
-      return false;
+      return true;
     }
   }
-
-
-
 }
