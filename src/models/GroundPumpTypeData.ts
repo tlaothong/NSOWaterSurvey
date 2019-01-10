c6c83add9340773b7510f92ae34356586ddd7e03
powerofwind <powerofwind10@gmail.com>
2019-01-10 11:28:42 +0700

    Implement Pump Ground Water

--- src/models/GroundPumpTypeData.ts
+++ src/models/GroundPumpTypeData.ts
@@ -5,13 +5,13 @@ export interface GroundWaterPumpType {
 }
 
 export const groundWaterPumpTypeData: GroundWaterPumpType[] = [
-    { codePowerSource: 'N1', codePumpType: 'N1P1', name: 'ปั๊มซับเมิร์ส/ปั๊มแบบจุ่ม' },
-    { codePowerSource: 'N1', codePumpType: 'N1P2', name: 'ปั๊มเจ็ท/ ปั๊มน้ำอัตโนมัติสำหรับดูดน้ำลึก' },
-    { codePowerSource: 'N1', codePumpType: 'N1P3', name: 'ปั๊มหอยโข่ง' },
-    { codePowerSource: 'N1', codePumpType: 'N1P4', name: 'ปั๊มชัก' },
-    { codePowerSource: 'N2', codePumpType: 'N2P1', name: 'ปั๊มหอยโข่ง' },
-    { codePowerSource: 'N3', codePumpType: 'N3P1', name: 'ปั๊มหอยโข่ง' },
-    { codePowerSource: 'N4', codePumpType: 'N4P1', name: 'ปั๊มหอยโข่ง' },
-    { codePowerSource: 'N4', codePumpType: 'N4P2', name: 'ปั๊มเจ็ท' },
-    { codePowerSource: 'N4', codePumpType: 'N4P3', name: 'เทอร์ไบร์' },
+    { codePowerSource: '1', codePumpType: 'N1P1', name: 'ปั๊มซับเมิร์ส/ปั๊มแบบจุ่ม' },
+    { codePowerSource: '1', codePumpType: 'N1P2', name: 'ปั๊มเจ็ท/ ปั๊มน้ำอัตโนมัติสำหรับดูดน้ำลึก' },
+    { codePowerSource: '1', codePumpType: 'N1P3', name: 'ปั๊มหอยโข่ง' },
+    { codePowerSource: '1', codePumpType: 'N1P4', name: 'ปั๊มชัก' },
+    { codePowerSource: '2', codePumpType: 'N2P1', name: 'ปั๊มหอยโข่ง' },
+    { codePowerSource: '3', codePumpType: 'N3P1', name: 'ปั๊มหอยโข่ง' },
+    { codePowerSource: '4', codePumpType: 'N4P1', name: 'ปั๊มหอยโข่ง' },
+    { codePowerSource: '4', codePumpType: 'N4P2', name: 'ปั๊มเจ็ท' },
+    { codePowerSource: '4', codePumpType: 'N4P3', name: 'เทอร์ไบร์' },
 ]
