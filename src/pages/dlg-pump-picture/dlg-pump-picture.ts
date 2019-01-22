5945de2fc75f7e8055b77512484d429e30e3b1f3
powerofwind <powerofwind10@gmail.com>
2019-01-22 12:29:43 +0700

    add more pump

--- src/pages/dlg-pump-picture/dlg-pump-picture.ts
+++ src/pages/dlg-pump-picture/dlg-pump-picture.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
 /**
  * Generated class for the DlgPumpPicturePage page.
@@ -15,11 +15,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DlgPumpPicturePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DlgPumpPicturePage');
+  }
+
+  public closeDialog() {
+    this.viewCtrl.dismiss();
   }
 
 }
