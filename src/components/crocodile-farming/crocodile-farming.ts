9a8d6b025cbb8d34350caa1cdde764abb4e121c2
powerofwind <powerofwind10@gmail.com>
2019-03-13 17:27:18 +0700

    hassamesize

--- src/components/crocodile-farming/crocodile-farming.ts
+++ src/components/crocodile-farming/crocodile-farming.ts
@@ -65,6 +65,13 @@ export class CrocodileFarmingComponent implements ISubmitRequestable {
 
   submitRequest() {
     this.submitRequested = true;
+    if (this.FormItem.get('fieldsAreSameSize').value) {
+      let val = this.FormItem.get('fields').value
+      for (let index = 1; index < val.length; index++) {
+        val[index] = val[0]
+      }
+      this.FormItem.get('fields').setValue(val)
+    }
     this.poolArea.forEach(it => it.submitRequest());
     this.waterSources9.forEach(it => it.submitRequest());
     // this.dispatchWaterSource();
