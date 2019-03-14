5cedbb05e753f91280446ca88fef410a525f2c93
powerofwind <powerofwind10@gmail.com>
2019-03-14 14:14:27 +0700

    pump value

--- src/models/GroundWattHpData.ts
+++ src/models/GroundWattHpData.ts
@@ -13,52 +13,52 @@ export const groundWaterWattHpData: GroundWaterWattHp[] = [
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W2',
-        name: '501-1000 / 0.7 - 1.5'
+        name: '501-1000 / 0.71 - 1.5'
     },
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W3',
-        name: '1001-1400 / 1.5 - 2'
+        name: '1001-1400 / 1.51 - 2'
     },
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W4',
-        name: '1401-2000 / 2 - 2.5'
+        name: '1401-2000 / 2.1 - 2.5'
     },
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W5',
-        name: '2001-3000 / 2.5 - 4'
+        name: '2001-3000 / 2.51 - 4'
     },
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W6',
-        name: '3001-5000 / 4- 7'
+        name: '3001-5000 / 4.1 - 7'
     },
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W7',
-        name: '5001-7000 / 7 - 9'
+        name: '5001-7000 / 7.1 - 9'
     },
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W8',
-        name: '7001-10000 / 9 - 15'
+        name: '7001-10000 / 9.1 - 15'
     },
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W9',
-        name: '10001-14000 / 15 - 20'
+        name: '10001-14000 / 15.1 - 20'
     },
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W10',
-        name: '15001-20000 / 20 - 25'
+        name: '15001-20000 / 20.1 - 25'
     },
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W11',
-        name: '50000-100000 / 65 - 135'
+        name: '20001-100000 / 25.1 - 135'
     },
     {
         codePumpType: 'N1P1',
@@ -98,72 +98,72 @@ export const groundWaterWattHpData: GroundWaterWattHp[] = [
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W2',
-        name: '501-1000 / 0.7 - 1.5'
+        name: '501-1000 / 0.71 - 1.5'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W3',
-        name: '1001-1400 / 1.5 - 2'
+        name: '1001-1500 / 1.51 - 2'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W4',
-        name: '1401-2000 / 2 - 2.5'
+        name: '1501-2000 / 21 - 2.5'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W5',
-        name: '2001-2500 / 2.5 - 3.5'
+        name: '2001-2500 / 2.51 - 3.5'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W6',
-        name: '2501-3500 / 3.5 - 4.5'
+        name: '2501-3500 / 3.51 - 4.5'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W7',
-        name: '3501-4500 / 4.5 - 6'
+        name: '3501-4500 / 4.51 - 6'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W8',
-        name: '4501-5500 / 6 - 7.5'
+        name: '4501-5500 / 6.1 - 7.5'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W9',
-        name: '5501-7500 / 7.5 - 10'
+        name: '5501-7500 / 7.51 - 10'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W10',
-        name: '7501-10000 / 10 - 15'
+        name: '7501-10000 / 10.1 - 15'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W11',
-        name: '10001-15000 / 15 - 20'
+        name: '10001-15000 / 15.1 - 20'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W12',
-        name: '15001-20000 / 20 - 25'
+        name: '15001-20000 / 20.1 - 25'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W13',
-        name: '20001-25000 / 25 - 35'
+        name: '20001-25000 / 25.1 - 35'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W14',
-        name: '25001-30000 / 35 - 40'
+        name: '25001-30000 / 35.1 - 40'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W15',
-        name: '30001-40000 / 40 - 55'
+        name: '>30000 / >40'
     },
     {
         codePumpType: 'N1P4',
@@ -173,52 +173,52 @@ export const groundWaterWattHpData: GroundWaterWattHp[] = [
     {
         codePumpType: 'N1P4',
         codeWattHp: 'N1P4W2',
-        name: '200-300 / 0.2 - 0.4'
+        name: '200-300 / 0.21 - 0.4'
     },
     {
         codePumpType: 'N1P4',
         codeWattHp: 'N1P4W3',
-        name: '301-400 / 0.4 - 0.5'
+        name: '301-400 / 0.41 - 0.5'
     },
     {
         codePumpType: 'N1P4',
         codeWattHp: 'N1P4W4',
-        name: '401-500 / 0.5 - 0.7'
+        name: '401-500 / 0.51 - 0.7'
     },
     {
         codePumpType: 'N1P4',
         codeWattHp: 'N1P4W5',
-        name: '501-700 / 0.7 - 1'
+        name: '501-700 / 0.71 - 1'
     },
     {
         codePumpType: 'N1P4',
         codeWattHp: 'N1P4W6',
-        name: '701-1000 / 1 - 1.5'
+        name: '701-1000 / 1.1 - 1.5'
     },
     {
         codePumpType: 'N1P4',
         codeWattHp: 'N1P4W7',
-        name: '1001-1400 / 1.5 - 2'
+        name: '1001-1400 / 1.51 - 2'
     },
     {
         codePumpType: 'N1P4',
         codeWattHp: 'N1P4W8',
-        name: '1401-2000 / 2 - 2.5'
+        name: '1401-2000 / 2.1 - 2.5'
     },
     {
         codePumpType: 'N1P4',
         codeWattHp: 'N1P4W9',
-        name: '2001-2500 / 2.5 - 3'
+        name: '2001-2500 / 2.51 - 3'
     },
     {
         codePumpType: 'N1P4',
         codeWattHp: 'N1P4W10',
-        name: '2501-3500 / 3 - 4.5'
+        name: '2501-3500 / 3.1 - 4.5'
     },
     {
         codePumpType: 'N1P4',
         codeWattHp: 'N1P4W11',
-        name: '3501-4500 / 4.5 - 6'
+        name: '>3500 / >4.5'
     },
     {
         codePumpType: 'N2P1',
@@ -228,42 +228,42 @@ export const groundWaterWattHpData: GroundWaterWattHp[] = [
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W2',
-        name: '4001-5000 / 5 - 7'
+        name: '4001-5000 / 5.1 - 7'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W3',
-        name: '5001-6000 / 7 - 8'
+        name: '5001-6000 / 7.1 - 8'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W4',
-        name: '6001-7000 / 8 - 9'
+        name: '6001-7000 / 8.1 - 9'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W5',
-        name: '7001-8000 / 9 - 11'
+        name: '7001-8000 / 9.1 - 11'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W6',
-        name: '8001-9000 / 11 - 12'
+        name: '8001-9000 / 11.1 - 12'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W7',
-        name: '9001-11000 / 12 - 15'
+        name: '9001-11000 / 12.1 - 15'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W8',
-        name: '11001-13500 / 15 - 18'
+        name: '11001-13500 / 15.1 - 18'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W9',
-        name: '14000-18000 / 19 - 24'
+        name: '13501-18000 / 18.1 - 24'
     },
     {
         codePumpType: 'N2P1',
@@ -278,32 +278,32 @@ export const groundWaterWattHpData: GroundWaterWattHp[] = [
     {
         codePumpType: 'N3P1',
         codeWattHp: 'N3P1W2',
-        name: '2501-3500 / 3 - 5'
+        name: '2501-3500 / 3.1 - 5'
     },
     {
         codePumpType: 'N3P1',
         codeWattHp: 'N3P1W3',
-        name: '3501-4000 / 5 - 5.5'
+        name: '3501-4000 / 5.1 - 5.5'
     },
     {
         codePumpType: 'N3P1',
         codeWattHp: 'N3P1W4',
-        name: '4001-4500 / 5.5 - 6'
+        name: '4001-4500 / 5.51 - 6'
     },
     {
         codePumpType: 'N3P1',
         codeWattHp: 'N3P1W5',
-        name: '4501-5000 / 6 - 7'
+        name: '4501-5000 / 6.1 - 7'
     },
     {
         codePumpType: 'N3P1',
         codeWattHp: 'N3P1W6',
-        name: '5001-6000 / 7 - 8'
+        name: '5001-6000 / 7.1 - 8'
     },
     {
         codePumpType: 'N3P1',
         codeWattHp: 'N3P1W7',
-        name: '6001-7000 / 8 - 9'
+        name: '6001-7000 / 8.1 - 9'
     },
     {
         codePumpType: 'N3P1',
@@ -318,52 +318,52 @@ export const groundWaterWattHpData: GroundWaterWattHp[] = [
     {
         codePumpType: 'N4P1',
         codeWattHp: 'N4P1W2',
-        name: '5500-6000 / 7 - 8'
+        name: '5500-6000 / 7.1 - 8'
     },
     {
         codePumpType: 'N4P1',
         codeWattHp: 'N4P1W3',
-        name: '6001-6500 / 8 - 9'
+        name: '6001-6500 / 8.1 - 9'
     },
     {
         codePumpType: 'N4P1',
         codeWattHp: 'N4P1W4',
-        name: '6501-7000 / 9 - 9.5'
+        name: '6501-7000 / 9.1 - 9.5'
     },
     {
         codePumpType: 'N4P1',
         codeWattHp: 'N4P1W5',
-        name: '7001-7500 / 9.5 - 10'
+        name: '7001-7500 / 9.51 - 10'
     },
     {
         codePumpType: 'N4P1',
         codeWattHp: 'N4P1W6',
-        name: '7501-8000 / 10 - 10.5'
+        name: '7501-8000 / 10.1 - 10.5'
     },
     {
         codePumpType: 'N4P1',
         codeWattHp: 'N4P1W7',
-        name: '8001-8500 / 10.5 - 11'
+        name: '8001-8500 / 10.51 - 11'
     },
     {
         codePumpType: 'N4P1',
         codeWattHp: 'N4P1W8',
-        name: '8501-9000 / 11 - 12'
+        name: '8501-9000 / 11.1 - 12'
     },
     {
         codePumpType: 'N4P1',
         codeWattHp: 'N4P1W9',
-        name: '9001-9500 / 12 - 13'
+        name: '9001-9500 / 12.1 - 13'
     },
     {
         codePumpType: 'N4P1',
         codeWattHp: 'N4P1W10',
-        name: '9501-10500 / 13 - 14'
+        name: '9501-10500 / 13.1 - 14'
     },
     {
         codePumpType: 'N4P1',
         codeWattHp: 'N4P1W11',
-        name: '10501-11500 / 14 - 15'
+        name: '10501-11500 / 14.1 - 15'
     },
     {
         codePumpType: 'N4P1',
@@ -383,17 +383,17 @@ export const groundWaterWattHpData: GroundWaterWattHp[] = [
     {
         codePumpType: 'N4P3',
         codeWattHp: 'N4P3W2',
-        name: '4001-5000 / 5 - 7'
+        name: '4001-5000 / 5.1 - 7'
     },
     {
         codePumpType: 'N4P3',
         codeWattHp: 'N4P3W3',
-        name: '5001-6000 / 7 - 8'
+        name: '5001-6000 / 7.1 - 8'
     },
     {
         codePumpType: 'N4P3',
         codeWattHp: 'N4P3W4',
-        name: '6001-7500 / 8 - 10'
+        name: '6001-7500 / 8.1 - 10'
     },
     {
         codePumpType: 'N4P3',
--- src/models/SurfaceWattHpData.ts
+++ src/models/SurfaceWattHpData.ts
@@ -13,62 +13,62 @@ export const surfaceWattHpData: surfaceWattHp[] = [
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W2',
-        name: '101-200 / 0.1 - 0.2'
+        name: '101-200 / 0.11 - 0.2'
     },
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W3',
-        name: '201-300 / 0.2 - 0.4'
+        name: '201-300 / 0.21 - 0.4'
     },
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W4',
-        name: '301-400 / 0.4 - 0.6'
+        name: '301-400 / 0.41 - 0.6'
     },
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W5',
-        name: '401-500 / 0.6 - 0.7'
+        name: '401-500 / 0.61 - 0.7'
     },
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W6',
-        name: '501-700 / 0.7 - 1'
+        name: '501-700 / 0.71 - 1'
     },
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W7',
-        name: '701-1000 / 1 - 1.5'
+        name: '701-1000 / 1.1 - 1.5'
     },
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W8',
-        name: '1001-1200 / 1.5 - 1.7'
+        name: '1001-1200 / 1.51 - 1.7'
     },
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W9',
-        name: '1201-1400 / 1.7 - 2'
+        name: '1201-1400 / 1.71 - 2'
     },
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W10',
-        name: '1401-2000 / 2 - 2.5'
+        name: '1401-2000 / 2.1 - 2.5'
     },
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W11',
-        name: '2001-3000 / 2.5 - 4'
+        name: '2001-3000 / 2.51 - 4'
     },
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W12',
-        name: '3001-5000 / 4 -7'
+        name: '3001-5000 / 4.1 -7'
     },
     {
         codePumpType: 'N1P1',
         codeWattHp: 'N1P1W13',
-        name: '5001-7000 / 7 - 9.5'
+        name: '5001-7000 / 7.1 - 9.5'
     },
     {
         codePumpType: 'N1P1',
@@ -83,32 +83,32 @@ export const surfaceWattHpData: surfaceWattHp[] = [
     {
         codePumpType: 'N1P2',
         codeWattHp: 'N1P2W2',
-        name: '101-200 / 0.1 - 0.3'
+        name: '101-200 / 0.11 - 0.3'
     },
     {
         codePumpType: 'N1P2',
         codeWattHp: 'N1P2W3',
-        name: '201-300 / 0.3 - 0.4'
+        name: '201-300 / 0.31 - 0.4'
     },
     {
         codePumpType: 'N1P2',
         codeWattHp: 'N1P2W4',
-        name: '301-400 / 0.4 - 0.5'
+        name: '301-400 / 0.41 - 0.5'
     },
     {
         codePumpType: 'N1P2',
         codeWattHp: 'N1P2W5',
-        name: '401-500 / 0.5 - 0.7'
+        name: '401-500 / 0.51 - 0.7'
     },
     {
         codePumpType: 'N1P2',
         codeWattHp: 'N1P2W6',
-        name: '501-700 / 0.7 - 0.9'
+        name: '501-700 / 0.71 - 0.9'
     },
     {
         codePumpType: 'N1P2',
         codeWattHp: 'N1P2W7',
-        name: '701-800 / 0.9 - 1'
+        name: '701-800 / 0.91 - 1'
     },
     {
         codePumpType: 'N1P2',
@@ -123,82 +123,82 @@ export const surfaceWattHpData: surfaceWattHp[] = [
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W2',
-        name: '401-600 / 0.5 - 1'
+        name: '401-600 / 0.51 - 1'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W3',
-        name: '601-1000 / 1 - 1.5'
+        name: '601-1000 / 1.1 - 1.5'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W4',
-        name: '1001-1400 / 1.5 - 2'
+        name: '1001-1400 / 1.51 - 2'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W5',
-        name: '1401-2000 / 2 - 3'
+        name: '1401-2000 / 2.1 - 3'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W6',
-        name: '2001-2500 / 3 - 3.5'
+        name: '2001-2500 / 3.1 - 3.5'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W7',
-        name: '2501-3500 / 3.5 - 5'
+        name: '2501-3500 / 3.51 - 5'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W8',
-        name: '3501-4500 / 5 - 6'
+        name: '3501-4500 / 5.1 - 6'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W9',
-        name: '4501-6000 / 6 - 8'
+        name: '4501-6000 / 6.1 - 8'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W10',
-        name: '6001-8000 / 8 - 11'
+        name: '6001-8000 / 8.1 - 11'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W11',
-        name: '8001-10000 / 11 - 15'
+        name: '8001-10000 / 11.1 - 15'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W12',
-        name: '10001-14000 / 15 - 20'
+        name: '10001-14000 / 15.1 - 20'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W13',
-        name: '14001-17000 / 20 - 23'
+        name: '14001-17000 / 20.1 - 23'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W14',
-        name: '17001-20000 / 23 - 25'
+        name: '17001-20000 / 23.1 - 25'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W15',
-        name: '20001-25000 / 25 - 35'
+        name: '20001-25000 / 25.1 - 35'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W16',
-        name: '30001-35000 / 40 - 47'
+        name: '25001-35000 / 35.1 - 47'
     },
     {
         codePumpType: 'N1P3',
         codeWattHp: 'N1P3W17',
-        name: '35001-40000 / 47 - 55'
+        name: '35001-40000 / 47.1 - 55'
     },
     {
         codePumpType: 'N1P3',
@@ -213,82 +213,77 @@ export const surfaceWattHpData: surfaceWattHp[] = [
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W2',
-        name: '4001-5000 / 5 - 7'
+        name: '4001-5000 / 5.1 - 7'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W3',
-        name: '5001-6000 / 7 - 8'
+        name: '5001-6000 / 7.1 - 8'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W4',
-        name: '6001-7000 / 8 - 9.5'
-    },
-    {
-        codePumpType: 'N2P1',
-        codeWattHp: 'N2P1W5',
-        name: '7001-8000 / 8 - 9.5'
+        name: '6001-7000 / 8.1 - 9.5'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W6',
-        name: '7001-8000 /  9.5 - 11'
+        name: '7001-8000 /  9.51 - 11'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W7',
-        name: '8001-9000 / 11 - 12'
+        name: '8001-9000 / 11.1 - 12'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W8',
-        name: '9001-10000 / 12 - 14'
+        name: '9001-10000 / 12.1 - 14'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W9',
-        name: '10001-12000 / 14 - 16'
+        name: '10001-12000 / 14.1 - 16'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W10',
-        name: '12001-15000 / 16-20'
+        name: '12001-15000 / 16.1 - 20'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W11',
-        name: '15001-20000 / 20 - 25'
+        name: '15001-20000 / 20.1 - 25'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W12',
-        name: '20001-30000 / 25 - 40'
+        name: '20001-30000 / 25.1 - 40'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W13',
-        name: '30001-40000 / 40 - 55'
+        name: '30001-40000 / 40.1 - 55'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W14',
-        name: '40001-50000 / 55 - 70'
+        name: '40001-50000 / 55.1 - 70'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W15',
-        name: '50001-60000 / 70 - 80'
+        name: '50001-60000 / 70.1 - 80'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W16',
-        name: '60001-80000 / 80 - 105'
+        name: '60001-80000 / 80.1 - 105'
     },
     {
         codePumpType: 'N2P1',
         codeWattHp: 'N2P1W17',
-        name: '80001-110000 / 105 -150'
+        name: '80001-110000 / 105.1 -150'
     },
     {
         codePumpType: 'N2P1',
@@ -303,32 +298,32 @@ export const surfaceWattHpData: surfaceWattHp[] = [
     {
         codePumpType: 'N3P1',
         codeWattHp: 'N3P1W2',
-        name: '4001-4500 / 5.5 - 6'
+        name: '4001-4500 / 5.51 - 6'
     },
     {
         codePumpType: 'N3P1',
         codeWattHp: 'N3P1W3',
-        name: '4501-5000 / 6 - 6.5'
+        name: '4501-5000 / 6.1 - 6.5'
     },
     {
         codePumpType: 'N3P1',
         codeWattHp: 'N3P1W4',
-        name: '5001-6000 / 6.5 - 8'
+        name: '5001-6000 / 6.51 - 8'
     },
     {
         codePumpType: 'N3P1',
         codeWattHp: 'N3P1W5',
-        name: '6001-7000 / 8 - 9.5'
+        name: '6001-7000 / 8.1 - 9.5'
     },
     {
         codePumpType: 'N3P1',
         codeWattHp: 'N3P1W6',
-        name: '7001-8000 / 9.5 - 10.5'
+        name: '7001-8000 / 9.51 - 10.5'
     },
     {
         codePumpType: 'N3P1',
         codeWattHp: 'N3P1W7',
-        name: '8001-10000 / 10.5 - 13'
+        name: '8001-10000 / 10.51 - 13'
     },
     {
         codePumpType: 'N3P1',
@@ -348,42 +343,42 @@ export const surfaceWattHpData: surfaceWattHp[] = [
     {
         codePumpType: 'N4P1',
         codeWattHp: 'N4P1W3',
-        name: '6001-6500 / 8 - 9'
+        name: '6001-6500 / 8.1 - 9'
     },
     {
         codePumpType: 'N4P1',
         codeWattHp: 'N4P1W4',
-        name: '6501-7000 /  9 - 9.5'
+        name: '6501-7000 /  9.1 - 9.5'
     },
     {
         codePumpType: 'N4P1',
         codeWattHp: 'N4P1W5',
-        name: '7001-8000 / 9.5 - 11'
+        name: '7001-8000 / 9.51 - 11'
     },
     {
         codePumpType: 'N4P1',
         codeWattHp: 'N4P1W6',
-        name: '8001-8500 / 11 - 11.5'
+        name: '8001-8500 / 11.1 - 11.5'
     },
     {
         codePumpType: 'N4P1',
         codeWattHp: 'N4P1W7',
-        name: '8501-9000 / 11.5 - 12'
+        name: '8501-9000 / 11.51 - 12'
     },
     {
         codePumpType: 'N4P1',
         codeWattHp: 'N4P1W8',
-        name: '9001-9500 / 12 - 13'
+        name: '9001-9500 / 12.1 - 13'
     },
     {
         codePumpType: 'N4P1',
         codeWattHp: 'N4P1W9',
-        name: '9501-11000 / 13 - 15'
+        name: '9501-11000 / 13.1 - 15'
     },
     {
         codePumpType: 'N4P1',
         codeWattHp: 'N4P1W10',
-        name: '11001-12000 / 15 - 16'
+        name: '11001-12000 / 15.1 - 16'
     },
     {
         codePumpType: 'N4P1',
