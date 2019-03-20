25dbbd5c87f8c0b0e5e1ab3401dd3eb4f78cdc50
unknown <nerddy.hahaha@hotmail.com>
2019-03-20 22:11:13 +0700

    add ngOnInit to show value

--- src/providers/location-data/location-data.ts
+++ src/providers/location-data/location-data.ts
@@ -3,12 +3,6 @@ import { Injectable } from '@angular/core';
 import { District, districtData } from '../../models/DistrictData';
 import { SubDistrict, subDistrictData } from '../../models/SubDistrictData';
 
-/*
-  Generated class for the LocationDataProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class LocationDataProvider {
 
