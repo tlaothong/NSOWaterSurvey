e70b6a2d9d6bb483c97f1b679a88264b5d0ccadf
woraput <woraput@kkumail.com>
2019-03-27 15:00:56 +0700

    refactor custom validate code commercialPage

--- src/pages/Commercial/Commercial.ts
+++ src/pages/Commercial/Commercial.ts
@@ -116,14 +116,10 @@ export class CommercialPage {
     this.waterSources8B.forEach(it => it.submitRequest());
     this.store.dispatch(new SetCommercialServiceType(this.f.get('serviceType').value));
     this.store.dispatch(new SetWaterSourcesCommercial(this.f.get('waterSources').value));
-    // this.dispatchWaterSource();
-    // this.dataCom.commerce = this.f.value
+    console.log(this.f);
+
     if (this.f.valid) {
       this.arrayIsCheckMethod();
-      // this.storage.set('unit', this.dataCom)
-      // let id = this.dataCom._id
-      // this.storage.set(id, this.dataCom)
-      // this.local.updateListUnit(this.dataCom.buildingId, this.dataCom)
       let originalHouseHold = this.appState.houseHoldUnit;
       let newHouseHold = {
         ...originalHouseHold,
@@ -136,6 +132,12 @@ export class CommercialPage {
 
   public static checkAnyOrOther(): ValidatorFn {
     return (c: AbstractControl): ValidationErrors | null => {
+      const preSchool = c.get('questionForAcademy.preSchool');
+      const kindergarten = c.get('questionForAcademy.kindergarten');
+      const primarySchool = c.get('questionForAcademy.primarySchool');
+      const highSchool = c.get('questionForAcademy.highSchool');
+      const vocational = c.get('questionForAcademy.vocational');
+      const higherEducation = c.get('questionForAcademy.higherEducation');
       const buildingCode = c.get('buildingCode');
       const personnelCountQuestionForAcademy = c.get('questionForAcademy.personnelCount');
       const roomCountHotelsAndResorts = c.get('hotelsAndResorts.roomCount');
@@ -148,6 +150,14 @@ export class CommercialPage {
       const peopleCount = c.get('religious.peopleCount');
       const personnelCountOtherBuilding = c.get('otherBuilding.personnelCount');
 
+      if (personnelCountQuestionForAcademy.value == null && (buildingCode.value == 11 || buildingCode.value == 12) && !preSchool.value.itemCount && !kindergarten.value.itemCount && !primarySchool.value.itemCount
+        && !highSchool.value.itemCount && !vocational.value.itemCount && !higherEducation.value.itemCount) {
+        return { 'anycheck': true, 'personnelCountQuestionForAcademy': true };
+      }
+      if ((buildingCode.value == 11 || buildingCode.value == 12) && !preSchool.value.itemCount && !kindergarten.value.itemCount && !primarySchool.value.itemCount
+        && !highSchool.value.itemCount && !vocational.value.itemCount && !higherEducation.value.itemCount) {
+        return { 'anycheck': true };
+      }
       if ((buildingCode.value == 11 || buildingCode.value == 12) && personnelCountQuestionForAcademy.value == null) {
         return { 'personnelCountQuestionForAcademy': true };
       }
@@ -244,7 +254,6 @@ export class CommercialPage {
         let arrLength = arrayNextPage.filter((it) => it == true);
         this.backNum = arrLength.length;
       }
-
     });
     let arrayIsCheck$ = this.store.select(getArrayIsCheck).pipe(map(s => s));
     let arrayIsCheck: any[];
