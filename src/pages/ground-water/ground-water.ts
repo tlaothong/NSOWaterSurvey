cf4adc4f4345a4229c2c11d3ddd303c7745d1070
powerofwind <powerofwind10@gmail.com>
2019-03-28 14:38:01 +0700

    validate ground water

--- src/pages/ground-water/ground-water.ts
+++ src/pages/ground-water/ground-water.ts
@@ -200,7 +200,7 @@ export class GroundWaterPage {
 
   public isCheckBoth(): boolean {
     if (this.isCheckActivity()) {
-      if ((this.f.get('privateGroundWater.doing').value && !this.f.get('publicGroundWater.doing').value) || (this.f.get('privateGroundWater.doing').value && this.f.get('publicGroundWater.doing').value)) {
+      if ((this.f.get('privateGroundWater.doing').value && !this.f.get('publicGroundWater.doing').value)) {
         return (this.f.get('privateGroundWater.waterResourceCount').value > 0);
       }
       else if (this.f.get('privateGroundWater.doing').value && this.f.get('publicGroundWater.doing').value) {
