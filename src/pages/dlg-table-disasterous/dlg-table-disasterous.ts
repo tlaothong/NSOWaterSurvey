8ca99c28e95b9b1b381e937def6fdc028ac0ff32
unravel07 <mick_ben10@hotmail.com>
2019-04-03 11:49:26 +0700

    checnge path form delete function

--- src/pages/dlg-table-disasterous/dlg-table-disasterous.ts
+++ src/pages/dlg-table-disasterous/dlg-table-disasterous.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, AlertController } from 'ionic-angular';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { TableDisasterousComponent } from '../../components/table-disasterous/table-disasterous';
 
@@ -13,7 +13,7 @@ export class DlgTableDisasterousPage {
   public FormItem: FormGroup;
   public text: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController, private fb: FormBuilder ) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController, private fb: FormBuilder, public alertController: AlertController) {
     this.FormItem = TableDisasterousComponent.CreateFormGroup(this.fb);
     const datain = navParams.get('FormItem') as FormGroup;
     this.FormItem.setValue(datain.value);
@@ -29,6 +29,31 @@ export class DlgTableDisasterousPage {
   }
   ionViewDidLoad() {
     console.log('ionViewDidLoad DlgTableDisasterousPage');
+  }
+
+  presentAlertPopulation() {
+    const alert = this.alertController.create({
+      title: 'คุณต้องการจะลบข้อมูลหรือไม่',
+      buttons: [
+        {
+          text: 'ยกเลิก',
+          handler: data => {
+
+          }
+        },
+        {
+          text: 'ยืนยัน',
+          handler: data => {
+            this.FormItem.reset();
+            this.okDialog();
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+  deleteData() {
+    this.presentAlertPopulation()
   }
 
   public isValid(name: string): boolean {
