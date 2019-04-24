50b491fc85a1b1fc42d965fa1b0f2554559b6aa6
unravel07 <mick_ben10@hotmail.com>
2019-04-24 11:52:09 +0700

    implement validate in residentalPage

--- src/pages/residential/residential.ts
+++ src/pages/residential/residential.ts
@@ -2,7 +2,7 @@ import { CountComponent } from './../../components/count/count';
 import { SetSelectorIndex, SaveHouseHold } from './../../states/household/household.actions';
 import { Component, ViewChildren } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, ValidatorFn, ValidationErrors, AbstractControl } from '@angular/forms';
 import { WaterSources8BComponent } from '../../components/water-sources8-b/water-sources8-b';
 import { Store } from '@ngrx/store';
 import { HouseHoldState } from '../../states/household/household.reducer';
@@ -22,7 +22,7 @@ export class ResidentialPage {
   private submitRequested: boolean;
   // private formData$ = this.store.select(getHouseHoldSample).pipe(map(s => s.residence));
   private formData$ = this.store.select(getHouseHoldSample);
-  public checked: boolean;
+  // public checked: boolean;
   private isCheckWarningBox: boolean;
   constructor(public navCtrl: NavController, private alertCtrl: AlertController, public navParams: NavParams, public fb: FormBuilder, private store: Store<HouseHoldState>, private appState: AppStateProvider) {
     this.residentialFrm = this.fb.group({
@@ -30,18 +30,9 @@ export class ResidentialPage {
       'workingAge': [null, Validators.compose([Validators.pattern('[0-9]*'), Validators.required])],
       'waterSources': WaterSources8BComponent.CreateFormGroup(this.fb),
       'gardeningUse': [null, Validators.required],
-    });
-  }
-
-  ionViewDidLoad() {
-
-  }
-
-  check(): boolean {
-    if (Number(this.residentialFrm.get('workingAge').value) > Number(this.residentialFrm.get('memberCount').value)) {
-      return this.checked = true
-    }
-    return this.checked = false
+    }, {
+        validator: ResidentialPage.checkAnyOrOther()
+      });
   }
 
   public handleSubmit() {
@@ -49,8 +40,10 @@ export class ResidentialPage {
     this.waterSources8B.forEach(it => it.submitRequest());
     this.count.forEach(it => it.submitRequest());
 
-    this.isCheckWarningBox = this.residentialFrm.valid && !(this.check());
-    if (this.residentialFrm.valid && !(this.check())) {
+    this.isCheckWarningBox = this.residentialFrm.valid;
+    console.log(this.residentialFrm);
+
+    if (this.residentialFrm.valid) {
       this.arrayIsCheckMethod();
       let originalHouseHold = this.appState.houseHoldUnit;
       let newHouseHold = {
@@ -61,47 +54,31 @@ export class ResidentialPage {
       this.store.dispatch(new SaveHouseHold(newHouseHold));
       this.navCtrl.popTo("CheckListPage");
     }
-    // else {
-    //   const doing = this.managementforfarming.get('doing').value;
-    //   const detaisInvalid = this.managementforfarming.get('details').invalid;
-    //   const projectCountValid = this.managementforfarming.get('projectCount').valid;
+  }
 
-    //   if (doing == false && projectCountValid && detaisInvalid) { // เข้าเงื่อนไขที่ยกเว้นได้
-    //     const confirmChanged = this.alertCtrl.create({
-    //       title: 'แก้ไขข้อมูลให้ถูกต้อง',
-    //       message: 'ไม่สามารถบันทึกรายการได้ เพราะมีข้อมูลรายละเอียดที่ไม่สมบูรณ์ <p>กด<b>ยืนยัน</b>หากท่านต้องการให้ระบบลบข้อมูลที่กรอกไว้เหล่านั้นทิ้ง แล้วกดบันทึกอีกครั้ง</p> <p>หรือกด<b>ยกเลิก</b>เพื่อกลับไปปรับปรุงข้อมูลด้วยตัวท่านเอง</p>',
-    //       buttons: [
-    //         "ยกเลิก",
-    //         {
-    //           text: "ยืนยัน",
-    //           handler: () => {
-    //             this.managementforfarming.get('projectCount').setValue(0);
-    //           },
-    //         },
-    //       ]
-    //     });
-    //     confirmChanged.present();
-    //   }
-    // }
+  public static checkAnyOrOther(): ValidatorFn {
+    return (c: AbstractControl): ValidationErrors | null => {
+      const workingAge = c.get('workingAge');
+      const memberCount = c.get('memberCount');
+
+      if (+workingAge.value > +memberCount.value) {
+        return { 'checkMember': true };
+      }
+      return null;
+    }
   }
 
   arrayIsCheckMethod() {
     this.store.dispatch(new SetSelectorIndex(0));
-    // let arrayIsCheck$ = this.store.select(getArrayIsCheck).pipe(map(s => s));
-    // let arrayIsCheck: Array<number>;
-    // arrayIsCheck$.subscribe(data => {
-    //   if (data != null) {
-    //     arrayIsCheck = data;
-    //     if (arrayIsCheck.every(it => it != 0)) {
-    //       arrayIsCheck.push(0);
-    //     }
-    //     console.log(arrayIsCheck);
-    //   }
-    // });
   }
 
   public isValid(name: string): boolean {
     var ctrl = this.residentialFrm.get(name);
+    if (name == 'checkMember') {
+      let ctrls = this.residentialFrm;
+      return ctrls.errors && ctrls.errors.checkMember && (ctrls.dirty || this.submitRequested);
+    }
+
     return ctrl.invalid && (ctrl.dirty || this.submitRequested);
   }
 }
