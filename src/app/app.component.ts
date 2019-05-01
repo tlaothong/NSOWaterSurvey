eac158ca18a8185d1cfb5e09ed4ee9bed4888047
Teerachai Laothong <tlaothong@gmail.com>
2019-05-01 13:12:59 +0700

    Alert and getIds()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,16 +48,16 @@ export class MyApp {
 
         this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
   
-        // this.oneSignal.getIds().then(p => {
-        //   // alert(JSON.stringify(p));
-        // });
+        this.oneSignal.getIds().then(p => {
+          alert("id:" + JSON.stringify(p));
+        });
   
         this.oneSignal.handleNotificationReceived().subscribe(v => {
-          // alert(JSON.stringify(v));
+          alert("rcv:" + JSON.stringify(v));
         });
   
         this.oneSignal.handleNotificationOpened().subscribe(v => {
-          // alert(JSON.stringify(v));
+          alert("open:" + JSON.stringify(v));
         });
   
         this.oneSignal.endInit();
