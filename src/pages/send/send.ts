2a993f97246ac2d1857f325691258f77e557cecc
thaninpong <thaninpong@perfenterprise.com>
2019-05-29 16:58:47 +0700

    edit ui

--- src/pages/send/send.ts
+++ src/pages/send/send.ts
@@ -220,8 +220,8 @@ export class SendPage {
     showDownload.addInput({
       type: 'checkbox',
       label: 'ต้องการทับงานของตัวเอง?',
-      value: 'value2',
-      checked: this.checkTub
+      value: 'true',
+      checked: false
     });
     showDownload.addButton('ยกเลิก');
     showDownload.addButton({
@@ -229,69 +229,73 @@ export class SendPage {
       handler: data => {
         // this.getUpload1.sessionId = this.getUpload1.sessionId + this.appState.deviceID
         console.log(this.getUpload1.sessionId);
-        this.cloudSync.downloadFromCloud1(this.getUpload1.sessionId).take(1).subscribe(async (data: donwloadBlob) => {
-          console.log(data);
-
-          for (const it of data.data) {
-            this.storeBoost.dispatch(new SetCurrentWorkingEA(it.ea));
-            await new Promise((rsv, rjt) => setTimeout(() => {
-              this.storeBoost.select(getCurrentWorkingEA).subscribe(async ea => {
-                if (it.ea == ea.code) {
-                  for (const it2 of it.items) {
-                    if (it2._id.startsWith("bld1v") || it2._id.startsWith("bld2v")) {
-
-                      let downloadUrl = data.baseUrl + it2.url + data.complementary;
-                      let cnt = await this.http.get<any>(downloadUrl).toPromise();
-                      this.storeBuilding.dispatch(new SetCurrentWorkingBuilding(cnt._id));
-                      await new Promise((rsv, rjt) => setTimeout(() => {
-
-                        this.storeBuilding.dispatch(new SaveBuilding(cnt));
-                        rsv({});
-
-                      }, 50));
-                    }
-                    if (it2._id.startsWith("unt1v") || it2._id.startsWith("unt2v")) {
-
-                      let downloadUrl = data.baseUrl + it2.url + data.complementary;
-                      let cnt = await this.http.get<any>(downloadUrl).toPromise();
-
-                      this.storeBuilding.dispatch(new SetCurrentWorkingBuilding(cnt.buildingId));
-                      await new Promise((rsv, rjt) => setTimeout(() => {
-
-                        this.storeHousehold.dispatch(new SaveHouseHold(cnt));
-                        rsv({});
-
-                      }, 50));
-                    }
-                    await new Promise((resvr, rjt) => setTimeout(resvr, 50));
-                  }
-
-                  rsv({});
-                }
-              });
-            }, 50));
-
-            for (const resol of it.resolutions) {
-              console.log(resol);
-              this.arrResol.push(resol);
-            }
-          }
-
-          this.cloudSync.downloadFromCloud2(this.getUpload1.sessionId).take(1).subscribe(async data => {
-            console.log("download2");
+        if (data.length == 0) {
+          this.cloudSync.downloadFromCloud1(this.getUpload1.sessionId).take(1).subscribe(async (data: donwloadBlob) => {
+            
             console.log(data);
+            for (const it of data.data) {
+              this.storeBoost.dispatch(new SetCurrentWorkingEA(it.ea));
+              await new Promise((rsv, rjt) => setTimeout(() => {
+                this.storeBoost.select(getCurrentWorkingEA).subscribe(async ea => {
+                  if (it.ea == ea.code) {
+                    for (const it2 of it.items) {
+                      if (it2._id.startsWith("bld1v") || it2._id.startsWith("bld2v")) {
+
+                        let downloadUrl = data.baseUrl + it2.url + data.complementary;
+                        let cnt = await this.http.get<any>(downloadUrl).toPromise();
+                        this.storeBuilding.dispatch(new SetCurrentWorkingBuilding(cnt._id));
+                        await new Promise((rsv, rjt) => setTimeout(() => {
+
+                          this.storeBuilding.dispatch(new SaveBuilding(cnt));
+                          rsv({});
+
+                        }, 50));
+                      }
+                      if (it2._id.startsWith("unt1v") || it2._id.startsWith("unt2v")) {
+
+                        let downloadUrl = data.baseUrl + it2.url + data.complementary;
+                        let cnt = await this.http.get<any>(downloadUrl).toPromise();
+
+                        this.storeBuilding.dispatch(new SetCurrentWorkingBuilding(cnt.buildingId));
+                        await new Promise((rsv, rjt) => setTimeout(() => {
+
+                          this.storeHousehold.dispatch(new SaveHouseHold(cnt));
+                          rsv({});
+
+                        }, 50));
+                      }
+                      await new Promise((resvr, rjt) => setTimeout(resvr, 50));
+                    }
+
+                    rsv({});
+                  }
+                });
+              }, 50));
+
+              for (const resol of it.resolutions) {
+                console.log(resol);
+                this.arrResol.push(resol);
+              }
+            }
+
+            this.cloudSync.downloadFromCloud2(this.getUpload1.sessionId).take(1).subscribe(async data => {
+              console.log("download2");
+              console.log(data);
+            });
+
+
           });
-
-
-        });
-
+        }
+        if (data == 'true') {
+          alert("Hello! I am an alert box!!");
+        }
         setTimeout(() => {
           const showDownloadsucess = this.alertCtrl.create();
           showDownloadsucess.setTitle('ดาวน์โหลดไฟล์');
           showDownloadsucess.setSubTitle('คุณได้ทำการดาวน์โหลดสำเร็จแล้ว');
           showDownloadsucess.addButton('ตกลง');
           showDownloadsucess.present();
-        }, 3000 );
+        }, 30000);
       }
     });
     showDownload.present();
