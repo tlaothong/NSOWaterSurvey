39c26ad4563050a616344053877fccaf0500480d
sikarin <so.sikarin@perfenterprise.com>
2019-06-13 23:28:56 +0700

    change dir

--- src/pages/export-file/export-file.ts
+++ src/pages/export-file/export-file.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
 import { File } from '@ionic-native/file/ngx';
 import { Storage } from '@ionic/storage';
 
@@ -9,9 +9,9 @@ import { Storage } from '@ionic/storage';
   templateUrl: 'export-file.html',
 })
 export class ExportFilePage {
-
+  
   constructor(public navCtrl: NavController, public navParams: NavParams, private file: File, private storage: Storage,
-    private alert: AlertController) {
+    private alert: AlertController, private load: LoadingController) {
   }
 
   ionViewDidLoad() {
@@ -19,19 +19,30 @@ export class ExportFilePage {
   }
 
   exportJSON() {
+    let alert = this.alert.create({
+      title: 'ไม่พบหน่วยความจำภายนอก',
+      buttons: ['ตกลง'],
+    });
+
+    let success = this.alert.create({
+      title: 'สำรองข้อมูลไปยังหน่วยความจำภายนอกเรียบร้อยแล้ว',
+      buttons: ['ตกลง'],
+    });
+
+    let load = this.load.create({
+      content: 'กำลังสำรองข้อมูลไปยังหน่วยความจำภายนอก',
+      enableBackdropDismiss: false,
+    });
+
     this.storage.keys().then(val => {
       let keys = val;
       console.log(keys);
-      let alert = this.alert.create({
-        title: 'ไม่พบหน่วยความจำภายนอก',
-        buttons: ['ตกลง'],
-      })
+
       for (let k of keys) {
         if (k.startsWith('ulogin1v')) {
           continue; // ignore login file
         }
         console.log(k);
-
         this.storage.get(k).then(val => {
           let txt = val;
           let fileName = k + '.txt';
@@ -41,142 +52,108 @@ export class ExportFilePage {
           console.log(txt);
           console.log(fileName);
           console.log(fileData);
-          this.file.createFile(this.file.applicationDirectory + "www/assets/", fileName, true)
-            .then((en) => {
-              this.file.writeFile(this.file.applicationDirectory + "www/assets/", fileName, fileData)
+          this.file.createDir(ROOT_DIRECTORY + folderName + '//', folderName, true)
+            .then((entries) => {
+              this.file.createFile(ROOT_DIRECTORY + folderName + '//', fileName, true)
                 .then((en) => {
-                  //Create a folder in memory location
-                  this.file.createDir(ROOT_DIRECTORY, folderName, true)
-                    .then((entries) => {
-                      //Copy our asset/img/FreakyJolly.jpg to folder we created
-                      this.file.copyFile(this.file.applicationDirectory + "www/assets/", fileName, ROOT_DIRECTORY + folderName + '//', fileName)
-                        .then((entries) => {
+                  this.file.writeFile(ROOT_DIRECTORY + folderName + '//', fileName, fileData)
+                    .then((en) => {
 
-                        })
-                        .catch((error) => {
-                          alert.present();
-                        });
                     })
                     .catch((error) => {
+                      load.dismiss();
                       alert.present();
-                    });
+                    })
                 })
                 .catch((error) => {
+                  load.dismiss();
                   alert.present();
                 })
             })
             .catch((error) => {
+              load.dismiss();
               alert.present();
-            })
-
+            });
         });
       }
+      load.dismiss();
+      success.present();
     });
+    load.present();
   }
 
   exportCSV() {
+    let load = this.load.create({
+      content: 'กำลังสำรองข้อมูลไปยังหน่วยความจำภายนอก',
+      enableBackdropDismiss: false,
+    });
+    let success = this.alert.create({
+      title: 'สำรองข้อมูลไปยังหน่วยความจำภายนอกเรียบร้อยแล้ว',
+      buttons: ['ตกลง'],
+    });
     this.storage.keys().then(val => {
       let keys = val;
       console.log(keys);
 
-      let bld = [];
-      let unt = [];
-      let alert = this.alert.create({
-        title: 'ไม่พบหน่วยความจำภายนอก',
-        buttons: ['ตกลง'],
-      })
       for (let k of keys) {
-        if (k.startsWith('ulogin1v')) {
-          continue; // ignore login file
-        }
-        console.log(k);
-        if (k.startsWith('unt1v')) {
+        if (k.startsWith('bldlst1v')) {
           this.storage.get(k).then(val => {
             let txt = val;
-            let fileName = k + '.txt';
-            let fileData = JSON.stringify(txt);
-            const ROOT_DIRECTORY = 'file:///sdcard//';
-            const folderName = 'water';
-            console.log(txt);
-            console.log(fileName);
-            unt.push(txt);
-            this.file.createFile(this.file.applicationDirectory + "www/assets/", fileName, true)
-              .then((en) => {
-                this.file.writeFile(this.file.applicationDirectory + "www/assets/", fileName, this.convertJsonToCsv(unt))
-                  .then((en) => {
-                    //Create a folder in memory location
-                    this.file.createDir(ROOT_DIRECTORY, folderName, true)
-                      .then((entries) => {
-                        //Copy our asset/img/FreakyJolly.jpg to folder we created
-                        this.file.copyFile(this.file.applicationDirectory + "www/assets/", fileName, ROOT_DIRECTORY + folderName + '//', fileName)
-                          .then((entries) => {
-
-                          })
-                          .catch((error) => {
-                            alert.present();
-                          });
-                      })
-                      .catch((error) => {
-                        alert.present();
-                      });
-                  })
-                  .catch((error) => {
-                    alert.present();
-                  })
-              })
-              .catch((error) => {
-                alert.present();
-              })
+            let fileDataBld = this.convertJsonToCsv(txt);
+            console.log(fileDataBld);
+            this.creatCSV('BuildingList', fileDataBld);
+           
           });
         }
-        if (k.startsWith('bld1v')) {
+        if (k.startsWith('unt4b1v')) {
           this.storage.get(k).then(val => {
             let txt = val;
-            let fileName = k + '.txt';
-            let fileData = JSON.stringify(txt);
-            const ROOT_DIRECTORY = 'file:///sdcard//';
-            const folderName = 'water';
-            console.log(txt);//JSON
-            console.log(fileName);
-            bld.push(txt);
-            this.file.createFile(this.file.applicationDirectory + "www/assets/", fileName, true)
-              .then((en) => {
-                this.file.writeFile(this.file.applicationDirectory + "www/assets/", fileName, this.convertJsonToCsv(bld))
-                  .then((en) => {
-                    //Create a folder in memory location
-                    this.file.createDir(ROOT_DIRECTORY, folderName, true)
-                      .then((entries) => {
-                        //Copy our asset/img/FreakyJolly.jpg to folder we created
-                        this.file.copyFile(this.file.applicationDirectory + "www/assets/", fileName, ROOT_DIRECTORY + folderName + '//', fileName)
-                          .then((entries) => {
-
-                          })
-                          .catch((error) => {
-                            alert.present();
-                          });
-                      })
-                      .catch((error) => {
-                        alert.present();
-                      });
-                  })
-                  .catch((error) => {
-                    alert.present();
-                  })
-              })
-              .catch((error) => {
-                alert.present();
-              })
+            let fileDataUnt = this.convertJsonToCsv(txt);
+            console.log(fileDataUnt);
+            this.creatCSV('HouseholdList', fileDataUnt);
+            
           });
         }
       }
-
+      load.dismiss();
+      success.present();
     });
+    load.present();
+  }
+
+  creatCSV(name: string, fileData) {
+    let alert = this.alert.create({
+      title: 'ไม่พบหน่วยความจำภายนอก',
+      buttons: ['ตกลง'],
+    });
+
+    let fileName = name + '.csv';
+    const ROOT_DIRECTORY = 'file:///sdcard//';
+    const folderName = 'water'
+    this.file.createDir(ROOT_DIRECTORY + folderName + '//', folderName, true)
+      .then((entries) => {
+        this.file.createFile(ROOT_DIRECTORY + folderName + '//', fileName, true)
+          .then((en) => {
+            this.file.writeFile(ROOT_DIRECTORY + folderName + '//', fileName, fileData)
+              .then((en) => {
+
+              })
+              .catch((error) => {
+                alert.present();
+              })
+          })
+          .catch((error) => {
+            alert.present();
+          })
+      })
+      .catch((error) => {
+        alert.present();
+      });
   }
 
   convertJsonToCsv(fileData) {
     var array = typeof fileData != 'object' ? JSON.parse(fileData) : fileData;
     console.log(array);
-
     var str = '';
     var row = "";
     for (var index in array[0]) {
